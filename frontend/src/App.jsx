7c5e1aa2d6c91549176c92500a0bead334cea508
Dumitrache Florentin <dumitracheflorentincristian@gmail.com>
2024-05-10 01:14:03 +0300

    feat: added toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
@@ -7,6 +10,19 @@ import Home from './pages/Home'
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />

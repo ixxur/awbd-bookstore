00caa4eaab4aca3c25004e9e836955df27ab1dd5
Ruxandra Oprea <ruxandranaoprea@yahoo.com>
2024-05-12 23:03:39 +0300

    added logs

--- backend/src/main/java/master/project/bookstore/service/BookService.java
+++ backend/src/main/java/master/project/bookstore/service/BookService.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Slf4j
+//@Slf4j
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
--- backend/src/main/java/master/project/bookstore/service/UserService.java
+++ backend/src/main/java/master/project/bookstore/service/UserService.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Slf4j
+//@Slf4j
 public class UserService {
     @Autowired
     private UserRepository userRepository;
